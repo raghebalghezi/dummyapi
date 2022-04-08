79e55d4c04533bda2d90ad89b54c95038dcc578e
Ragheb AlGhezi <raghebalghezi@gmail.com>
2022-04-08 10:09:18 +0300

    adding utils

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 from transformers import Wav2Vec2Processor, Wav2Vec2ForCTC
-
+from difflib import SequenceMatcher
 
 def model_loader(LANG):
   if LANG == 'fi':
@@ -11,3 +11,18 @@ def model_loader(LANG):
   return model, processor
 
 
+def make_tag(a: str, b: str) -> str:
+  new = "<p style='color: green'>"
+  s = SequenceMatcher(None, a, b)
+  for tag, i1, i2, j1, j2 in s.get_opcodes():
+    if tag == 'equal':
+      new += a[i1:i2]
+    elif tag ==  "delete":
+      new += f"<del style='color: red'>{a[i1:i2]}</del>"
+    elif tag == "insert":
+      new += f"<ins style='color: orange'>{b[j1:j2]}</ins>"
+    elif tag == "replace":
+      new += f"<del style='color: red'>{a[i1:i2]}</del>"
+      new += f"<ins style='color: orange'>{b[j1:j2]}</ins>"
+  new += "</p>"
+  return new
